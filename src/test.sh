fcdbfd7a8305e178a87bf1f44fca2edb0cf19e56
jpsm-at-deec <145997651+jpsm-at-deec@users.noreply.github.com>
2024-02-28 17:09:36 +0000

    Add files via upload

---
+++ src/test.sh
@@ -0,0 +1,5 @@
+#! /usr/bin/bash
+git clone https://github.com/jpsm-at-deec/ros2_segway_rmp.git
+git clone -b jammy https://github.com/jpsm-at-deec/libsegwayrmp.git 
+git clone https://github.com/jpsm-at-deec/segway_interfaces.git
+git clone https://github.com/jpsm-at-deec/serial.git
