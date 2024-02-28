b3688fd15270604f5a2d3e7340c403db61527633
jpsm-at-deec <145997651+jpsm-at-deec@users.noreply.github.com>
2024-02-28 17:28:56 +0000

    Update test.sh

--- src/test.sh
+++ src/test.sh
@@ -1,5 +1,5 @@
 #! /usr/bin/bash
-git clone https://github.com/jpsm-at-deec/ros2_segway_rmp.git
-git clone -b jammy https://github.com/jpsm-at-deec/libsegwayrmp.git 
-git clone https://github.com/jpsm-at-deec/segway_interfaces.git
-git clone https://github.com/jpsm-at-deec/serial.git
+git clone https://github.com/jpsm-at-deec/ros2_segway_rmp.git ./src/ros2_segway_rmp
+git clone -b jammy https://github.com/jpsm-at-deec/libsegwayrmp.git ./src/libsegwayrmp
+git clone https://github.com/jpsm-at-deec/segway_interfaces.git ./src/segway_interfaces
+git clone https://github.com/RoverRobotics-forks/serial-ros2.git ./src/serial
